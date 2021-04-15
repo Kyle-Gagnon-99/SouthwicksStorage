e18517df14adf707f58f48bf0d6aa8086880bb5a
kyle <kyle@DESKTOP-K34EGHD>
2021-04-15 18:17:12 -0400

    Initial Commit

---
+++ southwicksStorage/src/main/java/com/southwicksstorage/southwicksstorage/SouthwicksStorageApplication.java
@@ -0,0 +1,13 @@
+package com.southwicksstorage.southwicksstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SouthwicksStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SouthwicksStorageApplication.class, args);
+	}
+
+}
---
+++ southwicksStorage/src/test/java/com/southwicksstorage/southwicksstorage/SouthwicksStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.southwicksstorage.southwicksstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SouthwicksStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
