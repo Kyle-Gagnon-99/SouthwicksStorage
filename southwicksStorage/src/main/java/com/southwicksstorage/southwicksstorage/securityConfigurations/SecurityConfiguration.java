5d52e6ea9e365d3450e1127a6357cf6354771dd6
kyle <kyle@DESKTOP-K34EGHD>
2021-04-15 21:26:52 -0400

    Remove security for testing purposes

--- southwicksStorage/src/main/java/com/southwicksstorage/southwicksstorage/securityConfigurations/SecurityConfiguration.java
+++ southwicksStorage/src/main/java/com/southwicksstorage/southwicksstorage/securityConfigurations/SecurityConfiguration.java
@@ -31,7 +31,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests().antMatchers("/**").permitAll();
+		http.csrf().disable().authorizeRequests().anyRequest().permitAll();
 	}
 
 }
